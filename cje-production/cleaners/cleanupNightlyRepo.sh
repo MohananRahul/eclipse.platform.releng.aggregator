ee10b603e4dcb26501ffe59c9f0359ff1efebadc
Rahul Mohanan <rahul.mohanan@ibm.com>
2025-03-22 04:14:11 +0530

    lop

--- cje-production/cleaners/cleanupNightlyRepo.sh
+++ cje-production/cleaners/cleanupNightlyRepo.sh
@@ -3,7 +3,7 @@
 # Check if Java is installed
 if command -v java &> /dev/null
 then
-    echo "JAVA_HOME version: $JAVA_HOME/bin/java -version"
+     $JAVA_HOME/bin/java -version"
 else
     echo "Java is not installed."
 fi
