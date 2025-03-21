1de188248755624f2515b8f4a5b43d389ec65e65
Rahul Mohanan <rahul.mohanan@ibm.com>
2025-03-21 18:26:48 +0530

    jjp

--- cje-production/cleaners/cleanupNightlyRepo.sh
+++ cje-production/cleaners/cleanupNightlyRepo.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
 # Check if JAVA_HOME is set
-if [[ -z "${javaHome}" ]]; then
-  echo "JAVA_HOME is not set. Checking system default Java..."
-  javaCmd=$(which java)
-else
-  javaCmd=${javaHome}/bin/java
-fi
+JavaCMD=${JAVA_HOME}/bin/java
 
-# Verify if Java exists
-if [[ ! -x "${javaCmd}" ]]; then
-  echo "ERROR: Java executable not found!"
-  exit 1
-fi
 
 # Print Java version
-echo "Using Java from: ${javaCmd}"
-"${javaCmd}" -version
+echo "Using Java from: ${JavaCMD}"
+"${JavaCMD}" -version
